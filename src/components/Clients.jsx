f2f210df11ff2829a59cfb8a74f048d5f5a1407a
TrickySid <siddheshbakre46@gmail.com>
2023-07-07 12:51:40 +0530

    Commit

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -9,7 +9,7 @@ export default function Clients() {
     Aos.init({ duration: 1000 });
   }, []);
 
-  if (window.innerWidth <= 1280) {
+  if (window.innerWidth <= 980) {
     return (
       <div id="clients">
         <div className="clients-heading">Our Clients</div>
@@ -125,7 +125,7 @@ export default function Clients() {
         </div>
       </div>
     );
-  } else if (window.innerWidth > 1280) {
+  } else if (window.innerWidth > 980) {
     return (
       <div id="clients">
         <div className="clients-heading">Our Clients</div>
